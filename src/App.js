9246f8356101b55225875f13621fe5d9dd4cd5b8
Thiago Giammattey <giammattey@gmail.com>
2019-05-03 19:52:00 -0300

    withRouter higher order component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Food from './Food';
 import Meal from './Meal';
+import Navbar from './Navbar';
 import FoodSearch from './FoodSearch';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -9,6 +10,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Navbar />
 				<Switch>
 					<Route exact path="/food/:name" render={routeProps => <Food {...routeProps} />} />
 					<Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
